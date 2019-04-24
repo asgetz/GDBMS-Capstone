ea8ff2eb1d5e4667957454170f4d1ec211a0249a
alexgetz <asgetz@student.ysu.edu>
2019-04-24 18:52:27 -0400

    Updatad data file, and properly ignored

--- data/datagen.cpp
+++ data/datagen.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
-#define N 1000
+#define N 769240
 
 int main(){
 
@@ -13,17 +13,21 @@ int main(){
 
 	int out[N]={0};
 
-	std::ofstream fs("./data.txt");
+	std::ofstream fs("./data6.txt");
 	if(!fs){
 		std::cerr<<"Cannot open the output file."<<std::endl;
 	}
 	//fs<<"Entry_ID"<<std::setw(10)<<std::right<<"Age"<<std::endl;
 	for(int i=0;i<N;++i){
-		out[i]=rand()%80 + 1;
-		fs<<out[i]<<std::endl;
+		out[i]=rand()%1000 + 1;
+		fs<<i<<"    "<<out[i]<<std::endl;
 		//fs<<std::setw(4)<<i<<std::right<< ' ' << std::setw(14)<<out[i]<<std::left<<std::endl;
 	}
 	fs.close();
+
+
+	// std::string a = "769240    1000";
+	// std::cout<<"Size of last string is: "<<a.length()<<std::endl;
 
 	return 0;
 }
