f6a2b56243010840bcd92b1254f747afdb9f5c32
alexgetz <asgetz@student.ysu.edu>
2019-04-30 07:51:24 -0400

    Currently working on local system. Without Boost. Everything within kernel.cu

--- src/kernel.cu
+++ src/kernel.cu
@@ -1,118 +1,161 @@
 // kernel.cu
 // Alex Getz
 
-// #include <cuda_runtime_api.h>
-// #include <device_launch_parameters.h>
-// #include <cooperative_groups.h>
-// #include <crt/device_functions.h>
-// #include <fstream>
-
+#include <fstream>
+#include <cuda.h>
+#include <curand.h>
+#include <curand_kernel.h>
+#include <cuda_runtime.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <KernelInterface.h>
+// #include <KernelInterface.h>
+#include <assert.h>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <time.h>
+#include <math.h>
+#include <algorithm>
+#include <array>
 #include <assert.h>
 
-// #include <algorithm>
-// #include <iostream>
-// #include <sstream>
-// #include <cstring>
-// #include <string>
-// #include <vector>
+#define MAX_ENTRIES 11897027
+#define B_SIZE 2000
+#define TPB 128
 
-// #include <random>
-// #include <time.h>
-// #include <math.h>
-// #include <array>
+inline
+cudaError_t checkCuda(cudaError_t result)
+{
+#if defined(DEBUG) || defined(_DEBUG)
+    if(result!=cudaSuccess){
+        fprintf(stderr,"CUDA Runtime Error: %s\n",
+                cudaGetErrorString(result));
+        assert(result==cudaSuccess);
+    }
+#endif
+    return result;
+}
 
-
-// inline
-// cudaError_t checkCuda(cudaError_t result)
-// {
-// #if defined(DEBUG) || defined(_DEBUG)
-//     if(result!=cudaSuccess){
-//         fprintf(stderr,"CUDA Runtime Error: %s\n",
-//                 cudaGetErrorString(result));
-//         assert(result==cudaSuccess);
-//     }
-// #endif
-//     return result;
-// }
-//
-// #define CUDA_CALL(x) do { if((x) != cudaSuccess) { \
-//     printf("Error at %s:%d\n",__FILE__,__LINE__); \
-//     return EXIT_FAILURE;}} while(0)
+#define CUDA_CALL(x) do { if((x) != cudaSuccess) { \
+    printf("Error at %s:%d\n",__FILE__,__LINE__); \
+    return EXIT_FAILURE;}} while(0)
 
 
-// #define MUL(a, b) a*b
-//
-//
-// /* ///   Host Functions     /// */
-//
-//
-// /* ///   DEVICE FUNCTIONS   /// */
-//
-//
-// /* ///   GLOBAL FUNCTIONS   /// */
-// /**
-//  * [addKernel description]
-//  * @method addKernel
-//  * @param  c         [description]
-//  * @param  a         [description]
-//  * @param  b         [description]
-//  */
+#define MUL(a, b) a*b
+
+
+/* ///   Host Functions     /// */
+// __host__
+// void parkmillercall(unsigned int threads, unsigned int blocks);
+
+
+/* ///   DEVICE FUNCTIONS   /// */
+__device__ float getnextrand(curandState *state){
+    return (float)(curand_uniform(state));
+}
+
+__device__ int getnextrandscaled(curandState *state, int scale){
+    return (int) scale * getnextrand(state);
+}
+
+/* ///   GLOBAL FUNCTIONS   /// */
+/**
+ * [addKernel description]
+ * @method addKernel
+ * @param  c         [description]
+ * @param  a         [description]
+ * @param  b         [description]
+ */
 // __global__ void addKernel(int *c, const int *a, const int *b)
 // {
 //     int i = threadIdx.x;
 //     c[i] = a[i] + b[i];
 // }
-//
-//
-// /**
-//  * CUDA Kernel designated to carry out the bootstrap of the Base sample.
-//  * When launched, this kernel will generate a random number generator for
-//  * each thread that executes with it. These threads, with their random
-//  * number, will choose a random element from the original sample. Then the
-//  * kernel will construct an array of x_bar values by summation amongst the
-//  * threads.
-//  * @method bootstrap
-//  * @param  out       This is the output array, it will contain the x_bar values
-//  * @param  d_sample  This is the original sample that gets copied onto the GPU
-//  * @param  state     An array of pointers to unique cuRAND instances for threads
-//  * @return           return.
-//  */
-// __global__ void bootstrap(unsigned int *out, int *d_sample)
-// {
-//     /*  */
-//     // unsigned int tid = threadIdx.x;
-//     // unsigned int block = blockIdx.x;
-//     // unsigned int idx = threadIdx.x + (blockIdx.x*blockDim.x);
-//     // unsigned int ts;
-//     //
-//     // curandState localState = state[idx];
-//     // // curand_init(1234,0,idx,&localState);
-//     // // __syncthreads();
-//     //
-//     //
-//     // ts = curand(&localState);
-//     // out[idx]=d_sample[ts%100];
-//     // __syncthreads();
-//
-//     /*
-//      * Attempt to separate the blocks of 128 threads into
-//      * logical groupings of 100 for summation
-//      */
-//     // int x = idx%100;
-//     // int tx = idx-x;
-//     // int y = idx%128;
-//     // int ty = idx-y;
-//     // if(ty>tx){
-//     //     out[idx]=99999;
-//     // }else{
-//     //     out[idx]=(idx%100)+1;
-//     // }
+
+/**
+ * Device kernel to setup pseudo-random number generators that will work with
+ * the parallel nature of the GPU thread execution. In order for concurrent
+ * threads to generate numbers as intended, there must be a generator
+ * initialized for each individual thread.  That is what this kernel does.
+ *
+ * @method setup_kernel
+ * @param  state        A pointer to a certain generator spacific to an
+ *                      individual thread id.
+ */
+// __global__ void setup_kernel(curandState *state){
+//     /* Each thread gets same seed, a different sequence num, and no offset */
+//     int id = threadIdx.x + blockIdx.x * 64;
+//     curand_init(1234,id,0,&state[id]);
 // }
+
+/**
+ * Kernel to generate the random number sequence based off of the number
+ * generators initialized in the setup_kernel.
+ *
+ * @method generate_kernel
+ * @param  state           The pointer to a generator of a spacific thread
+ * @param  result          The random number generated w.r.t. the given state.
+ */
+// __global__ void generate_kernel(curandState *state, int *result){
+//     int id = threadIdx.x + blockIdx.x * 64; int count=0;
+//     unsigned int x;
 //
+//     /*Copies state into local mem, to avoid ineficiently accessing global mem*/
+//     curandState localState = state[id];
 //
+//     /*Generation of the pseudorandom numbers*/
+//     for(int n=0;n<100000;n++){
+//         x=curand(&localState);
+//         if(x & 1){ count++; }
+//     }
+//
+//     /*Back to global mem*/
+//     state[id] = localState;
+//
+//     /*Store results*/
+//     result[id]+=count;
+// }
+
+__global__ void initCurand(curandState *state, unsigned long seed){
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    curand_init(seed, idx, 0, &state[idx]);
+}
+
+/**
+ * CUDA Kernel designated to carry out the bootstrap of the Base sample.
+ * When launched, this kernel will generate a random number generator for
+ * each thread that executes with it. These threads, with their random
+ * number, will choose a random element from the original sample. Then the
+ * kernel will construct an array of x_bar values by summation amongst the
+ * threads.
+ * @method bootstrap
+ * @param  out       This is the output array, it will contain the x_bar values
+ * @param  d_sample  This is the original sample that gets copied onto the GPU
+ * @param  state     An array of pointers to unique cuRAND instances for threads
+ * @return           return.
+ */
+__global__ void bootstrap(int *c, int *d_sample, curandState *state)
+{
+    /*  */
+    unsigned int tid = threadIdx.x;
+    unsigned int block = blockIdx.x;
+    unsigned int idx = threadIdx.x + (blockIdx.x*blockDim.x);
+    unsigned int ts;
+    int temp = 0;
+    for(int i=0;i<MAX_ENTRIES;++i){
+        // ts = curand(&localState);
+        // temp+=d_sample[ts];
+        ts = getnextrandscaled(&state[idx], MAX_ENTRIES);
+        temp += d_sample[ts];
+    }
+    __syncthreads();
+    c[idx] = temp; // MAX_ENTRIES;
+}
+
+
 // /**
 //  * [parkmillerKernel description]
 //  * @method parkmillerKernel
@@ -150,7 +193,159 @@
 
 
 
+/**
+ * [main description]
+ * @method main
+ * @param  int  [description]
+ * @return      [description]
+ */
+int main(){
 
+    /* Variables have been offloaded into the KernelInterface class within
+     * kernel.h
+     * Most are public and/or generated by the class constructor
+     * So then create an opject to reference it by here.
+     */
+    //
+
+    int *BaseSample, *d_Base, *d_mean, *h_mean;
+    curandState *devStates;
+    checkCuda( cudaMallocHost((void**)&BaseSample,MAX_ENTRIES*sizeof(int)));
+    CUDA_CALL(cudaMalloc((void**)&devStates,2000*sizeof(curandState)));
+
+    // int *pData_h;
+    std::string line;
+
+    // pData_h = (int*)malloc(MAX_ENTRIES*sizeof(int));
+
+    uintmax_t m_numLines = 0;
+    std::ifstream fs("../data/allCountries.txt");
+    if(!fs){
+        /*std::cerr */
+        std::cout<<"ERROR\n";
+    }else{
+        while (std::getline(fs, line))
+        {
+            int counter=0;
+            std::stringstream ss;
+            std::string temp;
+            // std::cout<<"\n"<<line<<"\n";
+            ss << line;
+            std::getline(ss,temp,'\t');
+            // std::cout<<temp<<", position: "<<++counter<<"\n";
+            while(std::getline(ss,temp,'\t')){
+                if(temp.length() == 4){
+
+                    // Right now treat whole input stream as the sample
+                    // Later will add ability to distinguish what size of sample you want.
+                    // numEntries[i]=std::atoi(entries[i].c_str());
+                    // pData_h[m_numLines] = std::atoi(temp.c_str());
+
+                    // obj->BaseSample[m_numLines] = std::atoi(temp.c_str());
+                    BaseSample[m_numLines] = std::atoi(temp.c_str());
+
+
+
+                    // std::cout<<temp<<", position: "<<++counter<<"\n";
+                    break;
+                } else{ ++counter; }
+            }
+            m_numLines++;
+            // if(m_numLines==5){ break; }
+        }
+    }
+    std::cout << "m_numLines = " << m_numLines << "\nMoving on...\n\n";
+    fs.close();
+
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    // float chosen = 0.10;
+    // unsigned int blocksize = 128;
+    // unsigned int gridsize = 8192;
+    // int sample_n = 1048576;
+    // int N_offset = 10485760;
+    // int bootstrap_cycles = 2000;
+
+    // KernelInterface myObj (chosen, sample_n, N_offset, bootstrap_cycles);
+
+    // checkCuda( cudaMalloc((void**)&randArray,MAX_ENTRIES*sizeof(int)));
+
+    checkCuda( cudaMalloc((void**)&d_Base,MAX_ENTRIES*sizeof(int)));
+
+
+    checkCuda( cudaMemcpy(d_Base,BaseSample,MAX_ENTRIES*sizeof(int),cudaMemcpyHostToDevice));
+
+    checkCuda( cudaMalloc((void**)&d_mean,2000*sizeof(int)));
+    checkCuda( cudaMallocHost((void**)&h_mean,2000*sizeof(int)));
+
+
+    // parkmillerKernel<<<gridsize,blocksize>>>(randArray,1234,bootstrap_cycles,MAX_ENTRIES);
+    std::cout<<"Generated random numbers\nNow executing Bootstrap\n\n";
+    // 2048 bootstraps of 128 threads each
+    initCurand<<<(2000+128-1)/128,128>>>(devStates, 1234);
+    cudaDeviceSynchronize();
+    bootstrap<<<(2000+128-1)/128,128>>>(d_mean, d_Base, devStates);
+    cudaDeviceSynchronize();
+
+    checkCuda( cudaMemcpy(h_mean,d_mean,2000*sizeof(int),cudaMemcpyDeviceToHost));
+
+
+    for(int i=0;i<2000;++i){
+        std::cout<<"element "<<i<<" : "<<h_mean[i]<<std::endl;
+    }
+
+    cudaFree(d_Base);
+    cudaFree(d_mean);
+
+
+
+
+
+
+
+
+
+
+
+
+    //////////////////// Copy to Device, launch, and copy back to host
+    /* All of these steps have been offloaded onto the KernelInterface class */
+
+    //////////////////// Statistical analysis ; WILL OFFLOAD
+    /* At the very least calculate the pertinent bootstrap things
+     * Maybe even go further with histograms and charts through nvvp or something
+     */
+    //
+
+    //////////////////// Free veriables. Possibly invoke class deconstructor
+
+    printf("\n\n\nDONE\n\n\n");
+    cudaFree(devStates);
+    cudaFreeHost(h_mean);
+    cudaFreeHost(BaseSample);
+
+    // cudaFree(randArray);
+
+
+    return 0;
+}
+
+
+
+// /**
+//  * [parkmillercall description]
+//  * @method parkmillercall
+//  * @param  threads        [description]
+//  * @param  blocks         [description]
+//  */
+// void parkmillercall(unsigned int blocks, unsigned int threads, \
+//                     ){
+//     /**/
+//     checkCuda( cudaMalloc((void**)&randArray,PopSize*sizeof(int)));
+//     parkmillerKernel<<<blocks,threads>>>(randArray,1234,bStraps,PopSize);
+//     cudaFree(randArray);
+// }
 
 
 
@@ -328,3 +523,12 @@
 // cudaFree(d_mean);
 // cudaFreeHost(BaseSample);
 // cudaFreeHost(h_mean);
+
+
+
+/////// Allocate space for prng states on device
+////// Read in Population data and store it
+// using boost::iostreams::mapped_file_source;
+// using boost::iostreams::stream;
+// mapped_file_source mmap("../data/allCountries.txt");
+// stream<mapped_file_source> is(mmap, std::ios::binary);
