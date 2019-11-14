3407f03dc2264b4f0e0601b8692888dd740d9d63
asgetz <asgetz@student.ysu.edu>
2019-11-14 09:34:27 -0500

    Officially working now. RNG needs tweaking

--- src/kernel.cu
+++ src/kernel.cu
@@ -4,6 +4,7 @@
 #include <cstddef>
 #include <stdexcept>
 #include <memory>
+#include <cuda_profiler_api.h>
 
 #include <fstream>
 #include <cuda.h>
@@ -18,8 +19,9 @@
 #include <sstream>
 #include <cstring>
 #include <string>
-//#include <vector>
-//#include <time.h>
+
+#include <random>
+#include <functional>
 #include <math.h>
 #include <algorithm>
 #include <array>
@@ -27,7 +29,16 @@
 #define MAX_ENTRIES 11897026
 #define B_SIZE 2000
 #define TPB 128
+#define SHMEM_SIZE 1024 * sizeof(int)
 
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////
 inline
 cudaError_t checkCuda(cudaError_t result)
 {
@@ -52,9 +63,14 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 		if(abort) exit(code);
 	}
 }
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 
 
 /* ///   DEVICE FUNCTIONS   /// */
+
 __device__ float getnextrand(curandState *state){
     return (float)(curand_uniform(state));
 }
@@ -63,19 +79,24 @@ __device__ int getnextrandscaled(curandState *state, unsigned long int scale){
     return (unsigned long int) scale * getnextrand(state);
 }
 
+
+/* ///   DEVICE KERNELS   /// */
+
 __global__ void initCurand(curandState *state, unsigned long seed){
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
     curand_init(idx+seed, 0, 0, &state[idx]);
 }
 
-__global__ void bootstrap(int *output_mean, int *d_sample, curandState *state)
+__global__ void bootstrap(double *output_mean, int *d_sample, curandState *state)
 {
+    __shared__ int partial_Sums[SHMEM_SIZE];
+
     unsigned int idx = threadIdx.x + (blockIdx.x*blockDim.x);
     unsigned int tNum = threadIdx.x;
-    unsigned int bSize = blockDim.x;
+    int bNum = blockIdx.x;
+    int bSize = blockDim.x;
     unsigned long int ts = 0;
     long long int tSum = 0;
-    __shared__ int partial_Sums[1024];
     int count = 0;
 
     for(unsigned int i=tNum; i<MAX_ENTRIES; i+=bSize){
@@ -83,32 +104,35 @@ __global__ void bootstrap(int *output_mean, int *d_sample, curandState *state)
 	tSum += d_sample[ts];
 	count++;
     }
-
-    partial_Sums[tNum] = tSum/count;
+    partial_Sums[tNum] = tSum / count;
     __syncthreads();
-    
-    
-	
-	
-    /*
-    unsigned int idx = threadIdx.x + (blockIdx.x*blockDim.x);
-    unsigned long int ts;
-    long long int sum = 0;
-    for(int i=0;i<(MAX_ENTRIES/100);++i){
-        ts = getnextrandscaled(&state[idx], MAX_ENTRIES);
-        sum += d_sample[ts];
+
+    // Perform Sum reduction across all the threads of the block
+    for(int s=(bSize/2); s>0; s >>= 1){
+	// Each thread does work unless the index goes off the block
+	if(tNum<s){
+	    partial_Sums[tNum] += partial_Sums[tNum+s];
+	}
+	__syncthreads();
     }
-    output_mean[idx] = (sum/(MAX_ENTRIES/100));
-    */
-    
+
+    // Use first thread of each block to write results back to main mem
+    if(tNum==0){
+        output_mean[bNum] = (double)partial_Sums[0] / (double)bSize;
+    }
 }
 
 
+
+/* ///   HOST GLOBAL FUNCTIONS   /// */
 
 void throw_error(cudaError_t err){
     if(err != cudaSuccess)
 	throw std::runtime_error(cudaGetErrorString(err));
 }
+
+
+/* ///   HOST GLOBAL STRUCTS & VARIABLES   /// */
 
 struct cuda_free_deleter_t{
     void operator()(void* ptr) const
@@ -126,24 +150,17 @@ auto cudaAllocBuffer(std::size_t size){
 }
 
 
-
 int main(){
-
     int *BaseSample, *d_Base;
-    int *d_mean, *h_mean;
+    double *d_mean, *h_mean;
     //curandState *devStates;
     checkCuda( cudaMallocHost((void**)&BaseSample,MAX_ENTRIES*sizeof(int)));
     //checkCuda( cudaMalloc((void**)&devStates,2048*1024*sizeof(curandState)));
 
-    // int *pData_h;
     std::string line;
-
-    // pData_h = (int*)malloc(MAX_ENTRIES*sizeof(int));
-
     uintmax_t m_numLines = 0;
     std::ifstream fs("../data/allCountries.txt");
     if(!fs){
-        /*std::cerr */
         std::cout<<"ERROR\n";
     }else{
         while (std::getline(fs, line))
@@ -175,17 +192,33 @@ int main(){
     //std::cout << "Element 300,000 of BaseSample: " << BaseSample[300000]<<std::endl;
 
 
+    ///---			Calculating the Mean			 ---///
+    ///////////////////////////////////////////////////////////////////////////
+    std::cout<<"Sample has been generated.\nCalculating the mean...\n";
+    long long int BaseSum = 0;
+    for(int i=0;i<MAX_ENTRIES;i++){
+	BaseSum += BaseSample[i];
+    }
+    double BaseMean = (double)BaseSum / (double)MAX_ENTRIES;
+    std::cout<<"Mean has been calculated! Moving on...\n\n";
+    
+
+
+    ///---			KERNEL OPERATIONS			 ---///
     ///////////////////////////////////////////////////////////////////////////
     checkCuda( cudaMalloc((void**)&d_Base,MAX_ENTRIES*sizeof(int)));
     checkCuda( cudaMemcpy(d_Base,BaseSample,MAX_ENTRIES*sizeof(int),cudaMemcpyHostToDevice));
-    checkCuda( cudaFreeHost(BaseSample) );
 
-    checkCuda( cudaMalloc((void**)&d_mean,2048*sizeof(int)));
-    checkCuda( cudaMallocHost((void**)&h_mean,2048*sizeof(int)));
 
-    //std::cout<<"Launching initCurand Kernel now\n\n";
+    //checkCuda( cudaFreeHost(BaseSample) );
+
+    checkCuda( cudaMalloc((void**)&d_mean,2048*sizeof(double)));
+    checkCuda( cudaMallocHost((void**)&h_mean,2048*sizeof(double)));
+
+    std::cout<<"Launching initCurand Kernel now\n\n";
 
     //////////////////////////////////////
+    //checkCuda( cudaProfilerStart() );
     try{
 	constexpr int block_size = 512;
 	constexpr int num_blocks = 4096;
@@ -193,12 +226,12 @@ int main(){
 	initCurand<<<num_blocks, block_size>>>(devStates.get(),1234);
 	throw_error(cudaPeekAtLastError());
 	throw_error(cudaDeviceSynchronize());
-	//std::cout<<"Curand Kernel Launch Try block SUCCESSFUL!\n";
-	//std::cout<<"Launching Bootstrap Kernel now\n\n";
+	std::cout<<"Curand Kernel Launch Try block SUCCESSFUL!\n";
+	std::cout<<"Launching Bootstrap Kernel now\n\n";
 	bootstrap<<<2048,1024>>>(d_mean,d_Base,devStates.get());
 	throw_error(cudaPeekAtLastError());
 	throw_error(cudaDeviceSynchronize());
-	//std::cout<<"Bootstrap Kernel Launch Try Block SUCCESSFUL!\n";
+	std::cout<<"Bootstrap Kernel Launch Try Block SUCCESSFUL!\n";
     }
     catch (const std::exception& e)
     {
@@ -211,37 +244,73 @@ int main(){
 	return -1;
     }
 
-    //initCurand<<<2048,1024>>>(devStates, 1234);
-    //gpuErrchk( cudaPeekAtLastError() );
-    //gpuErrchk( cudaDeviceSynchronize() );
+    std::cout<<"Kernels appear complete, attempting to copy data back to Host\n";
+    checkCuda( cudaMemcpy(h_mean,d_mean,2048*sizeof(double),cudaMemcpyDeviceToHost) );
 
-    //std::cout<<"Launching bootstrap Kernel now\n\n";
-
-    //bootstrap<<<2048,1024>>>(d_mean, d_Base, devStates);
-    //std::cout<<"Bootstrap Kernel launched; checking for errors & synching threads\n";
-    //checkCuda( cudaPeekAtLastError() );
-    //checkCuda( cudaDeviceSynchronize() );
-    //std::cout<<"Bootstrap Kernel threads should be synched\n";
-
-    //std::cout<<"Kernels appear complete, attempting to copy data back to Host\n";
-    //checkCuda( cudaMemcpy(h_mean,d_mean,2048*sizeof(int),cudaMemcpyDeviceToHost) );
-
-    /*
+    /* This loop is meant for testing the validity of the memcpy output
     for(int i=0;i<2048;++i){
         std::cout<<"element "<<i<<" : "<<h_mean[i]<<std::endl;
     }
     */
+
+    // Standard Error of the bootstrap means
+    int n = 2048;
+    double SumOfMeans=0;
+    for(int i=0;i<n;i++){
+	SumOfMeans+=h_mean[i];
+    }
+    double MeanOfMeans = SumOfMeans / (double)n;
+    std::cout<<"MeanofMeans: "<<MeanOfMeans<<"\n";
+
+    double SqrDiff=0;
+    for(int i=0;i<n;i++){
+	SqrDiff += (h_mean[i]-MeanOfMeans) * (h_mean[i]-MeanOfMeans);
+    }
+    double SqrdVariance = SqrDiff / (n-1);
+    double BootError = ((n-1)/(n*n)) * SqrdVariance;
+    std::cout<<"SqrDiff, SqrdVariance, & BootError: "<<SqrDiff<<", "<<SqrdVariance<<", "<<BootError<<"\n";
+
+    //std::sort(h_mean,h_mean+2048);
+    std::cout<<"\nStandard Error is: "<<BootError<<"\n\n\n";
+
+    double C_Arr[2048] = {};
+    for(int i=0;i<n;i++){
+	C_Arr[i]=h_mean[i]-BaseMean;
+    }
+    std::sort(std::begin(C_Arr),std::end(C_Arr));
+    double L = 2048.0 * 0.1;
+    int Lower = (int)L;
+    double H = 2048 * 0.9;
+    int Higher = (int)H;
+
+    int LowerBound = C_Arr[Lower];
+    int UpperBound = C_Arr[Higher];
+
+    double Left = BaseMean - (double)LowerBound;
+    double Right = BaseMean - (double)UpperBound;
+
+    std::cout<<"\n\n\n------------------------------------------------\n";
+    std::cout<<"The Confidence Interval is: 80%\n";
+    std::cout<<"The Standard Error is: "<<BootError<<"\n";
+    std::cout<<"This is on the 10th & 90th percentiles: ["<<Left<<", "<<Right<<"]\n";
 
     checkCuda( cudaFree(d_Base) );
     checkCuda( cudaFree(d_mean) );
     //checkCuda( cudaFree(devStates) );
     checkCuda( cudaFreeHost(BaseSample) );
     checkCuda( cudaFreeHost(h_mean) );
-    printf("DONE");
+    printf("\n\n\nDONE\n");
 
 
     return 0;
 }
+
+
+
+
+
+
+
 
 
 //////////////////// Calculate Statistics. Soon to be offloaded
